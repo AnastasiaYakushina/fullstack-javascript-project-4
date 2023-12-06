4f3600367e415cb1dac990cae9199cd347af4e5f
AnastasiaYakushina <aubarr@ya.ru>
2023-12-06 22:00:29 +0300

    feat: update download.js & page-loader.js

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import download from '../src/download.js';
 
 program
   .description('Page loader utility')
   .version('1.0.0')
   .argument('url')
   .option('-o, --output [dir]', 'output dir (default: "/home/user/current-dir")')
-  .action((url) => {
-    console.log(url);
+  .action((url, options) => {
+    download(url, options.output);
   });
 
 program.parse();
--- src/download.js
+++ src/download.js
@@ -5,10 +5,18 @@ import process from 'process';
 
 const workingDir = process.cwd();
 
+const generateName = (url) => {
+  const formatUrl = url.replace(/https?:\/\//, '').replace(/[\W_]/g, '-');
+  return `${formatUrl}.html`;
+};
+
 const download = (url, outputPath = workingDir) => {
-  const dest = path.join(outputPath, '/en-wikipedia-org-wiki-Asynchrony--computer-programming-.html');
+  const dest = path.join(outputPath, generateName(url));
+  console.log(dest);
   return axios.get(url)
     .then((result) => fs.writeFile(dest, result.data));
 };
 
 export default download;
+
+
