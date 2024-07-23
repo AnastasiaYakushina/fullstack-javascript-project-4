a6037325f8da697b67ae0a2fa947f453512d7474
AnastasiaYakushina <aubarr@ya.ru>
2024-07-23 15:07:40 +0300

    Fix createName.js: delete double

--- src/createName.js
+++ src/createName.js
@@ -6,8 +6,9 @@ export default (url, type = 'file') => {
   const objectUrl = new URL(url);
   const { host, pathname } = objectUrl;
   const { dir, name, ext } = path.parse(pathname);
+  const newDir = (dir !== '/') ? `${dir}/` : dir;
   if (type === 'dir') {
-    return `${replace(host)}${replace(dir)}${replace(name)}_files`;
+    return `${replace(host)}${replace(newDir)}${replace(name)}_files`;
   }
-  return (ext) ? `${replace(host)}${replace(dir)}-${replace(name)}${ext}` : `${replace(host)}${replace(dir)}${replace(name)}.html`;
+  return (ext) ? `${replace(host)}${replace(newDir)}${replace(name)}${ext}` : `${replace(host)}${replace(newDir)}${replace(name)}.html`;
 };
